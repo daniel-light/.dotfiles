31d415694194cb4424e81fa5461f7972e3ec1b34
Daniel Light <dsl7v3@mail.missouri.edu>
2014-04-22 21:13:45 +0000

    Remove explicit file lists in script, move all files in the subdirs instead

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 set -x #-o xtrace
 set -u #-o nounset
+set -e #-o errexit
 
 
 dir="$HOME/.dotfiles"
 subdirdot='dot'
 subdirnot='not'
 olddir='old'
-dotfiles='vimrc zshrc zlogin gemrc'
-notfiles=''
 
 #mkdir -p $olddir && echo "Creating $olddir"
 mkdir -p $dir/{$olddir,$subdirdot,$subdirnot}
 
 cd $dir
 cd $subdirdot
-for file in $dotfiles; do
+for file in $(ls); do
   mv ~/.$file $dir/$olddir && echo "Backing up ~/.$file to $dir/$olddir/.$file"
   ln -s $dir/$subdirdot/$file ~/.$file && echo "Link ~/.$file to $dir/$subdirdot/$file"
 done
 
 cd $dir
 cd $subdirnot
-for file in $notfiles; do
+for file in $(ls); do
   mv ~/$file $dir/$olddir && echo "Backing up ~/$file to $dir/$olddir/$file"
   ln -s $dir/$subdirnot/$file ~/$file && echo "Link ~/$file to $dir/$subdirnot/$file"
 done
