244fd71ded1741108889e37c3a2d5da8bfaa2fb0
Daniel Light <dsl7v3@mail.missouri.edu>
2014-04-25 03:45:39 +0000

    Test to avoid backing up and recreating identical links

--- install.sh
+++ install.sh
@@ -14,13 +14,17 @@ mkdir -p $dir/$subdirdot $dir/$subdirnot $dir/$olddir && echo 'Creating director
 cd $dir
 cd $subdirdot
 for file in $(ls); do
-  mv ~/.$file $dir/$olddir && echo "Backing up ~/.$file to $dir/$olddir/.$file"
-  ln -s $dir/$subdirdot/$file ~/.$file && echo "Link ~/.$file to $dir/$subdirdot/$file"
+  if [ $(readlink $HOME/.$file) != $dir/$subdirdot/$file ]; then
+    mv ~/.$file $dir/$olddir && echo "Backing up ~/.$file to $dir/$olddir/.$file"
+    ln -s $dir/$subdirdot/$file ~/.$file && echo "Link ~/.$file to $dir/$subdirdot/$file"
+  fi
 done
 
 cd $dir
 cd $subdirnot
 for file in $(ls); do
-  mv ~/$file $dir/$olddir && echo "Backing up ~/$file to $dir/$olddir/$file"
-  ln -s $dir/$subdirnot/$file ~/$file && echo "Link ~/$file to $dir/$subdirnot/$file"
+  if [ $(readlink $HOME/$file) != $dir/$subdirnot/$file ]; then
+    mv ~/$file $dir/$olddir && echo "Backing up ~/$file to $dir/$olddir/$file"
+    ln -s $dir/$subdirnot/$file ~/$file && echo "Link ~/$file to $dir/$subdirnot/$file"
+  fi
 done
