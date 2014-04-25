f7e01b736a5194658159d6f941d6eb682aa39cb9
Daniel Light <dsl7v3@mail.missouri.edu>
2014-04-25 15:38:19 +0000

    Move all files to be synchronized to a single content folder

--- install.sh
+++ install.sh
@@ -5,26 +5,15 @@ set -e #-o errexit
 
 
 dir="$HOME/.dotfiles"
-subdirdot='dot'
-subdirnot='not'
+contentdir='files'
 olddir='old'
 
-mkdir -p $dir/$subdirdot $dir/$subdirnot $dir/$olddir && echo 'Creating directory structure'
+mkdir -p $dir/$contentdir $dir/$olddir && echo 'Creating directory structure'
 
-cd $dir
-cd $subdirdot
-for file in $(ls); do
-  if [ $(readlink $HOME/.$file) != $dir/$subdirdot/$file ]; then
-    mv ~/.$file $dir/$olddir && echo "Backing up ~/.$file to $dir/$olddir/.$file"
-    ln -s $dir/$subdirdot/$file ~/.$file && echo "Link ~/.$file to $dir/$subdirdot/$file"
-  fi
-done
-
-cd $dir
-cd $subdirnot
-for file in $(ls); do
-  if [ $(readlink $HOME/$file) != $dir/$subdirnot/$file ]; then
+cd $dir/$contentdir
+for file in $(ls -A); do
+  if [ $(readlink $HOME/$file) != $dir/$contentdir/$file ]; then
     mv ~/$file $dir/$olddir && echo "Backing up ~/$file to $dir/$olddir/$file"
-    ln -s $dir/$subdirnot/$file ~/$file && echo "Link ~/$file to $dir/$subdirnot/$file"
+    ln -s $dir/$contentdir/$file ~/$file && echo "Link ~/$file to $dir/$contentdir/$file"
   fi
 done
