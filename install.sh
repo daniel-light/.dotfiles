e663ef405740b53a8623c7b9401d0781bd3dcb7b
Daniel Light <dsl7v3@mail.missouri.edu>
2014-04-27 04:13:58 +0000

    Fix error on testing a nonexistant link

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p $dir/$contentdir $dir/$olddir && echo 'Creating directory structure'
 
 cd $dir/$contentdir
 for file in $(ls -A); do
-  if [ $(readlink $HOME/$file) != $dir/$contentdir/$file ]; then
+  if [ "$(readlink $HOME/$file)" != "$dir/$contentdir/$file" ]; then
     mv ~/$file $dir/$olddir && echo "Backing up ~/$file to $dir/$olddir/$file"
     ln -s $dir/$contentdir/$file ~/$file && echo "Link ~/$file to $dir/$contentdir/$file"
   fi
