f918b77ab1954d9b0e797a2730a5723c7e902e96
Daniel Light <daniel.sj.light@gmail.com>
2015-09-04 11:19:59 -0400

    Add fzf shell integrations

---
+++ files/.fzf.zsh
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" =~ "/Users/daniellight/.dotfiles/build/fzf/bin" ]]; then
+  export PATH="$PATH:/Users/daniellight/.dotfiles/build/fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "/Users/daniellight/.dotfiles/build/fzf/man" && -d "/Users/daniellight/.dotfiles/build/fzf/man" ]]; then
+  export MANPATH="$MANPATH:/Users/daniellight/.dotfiles/build/fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "/Users/daniellight/.dotfiles/build/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/daniellight/.dotfiles/build/fzf/shell/key-bindings.zsh"
+
