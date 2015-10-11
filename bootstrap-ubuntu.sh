a4699654e4ae6d046065613132138a38e5720dab
Daniel Light <daniel.sj.light@gmail.com>
2015-10-11 15:19:33 -0400

    Add some more packages to bootstrap

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -8,5 +8,9 @@ sudo add-apt-repository ppa:webupd8team/java
 
 sudo apt-get update
 
-sudo apt-get install -y neovim git oracle-java8-installer \ # from the ppas
+sudo apt-get install -y neovim \ # ppa:neovim-ppa
+						git \ # ppa:git-core
+						oracle-java8-installer \ # ppa:webupd8team
+						newsbeuter \ # random userland
 						postgresql libpq-dev
+						avrdude gcc-avr avr-libc emacs jq # for atreus firmware
