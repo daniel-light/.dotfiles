c0afab03a6bba4fef98e639647371d544543c23b
Daniel Light <daniel.sj.light@gmail.com>
2015-10-11 15:34:53 -0400

    Do some work on the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,35 @@
 set -e
 set -n
 
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:webupd8team/java
+# inner arrays are laid out as: ubuntu, 
+TARGETS=(
+	(neovim)
+	(git)
+	(oracle-java8-installer)
+	(newsbeuter)
+	(postgresql)
+	(libpq-dev)
+	(avrdude)
+	(avr-libc)
+	(emacs)
+	(jq)
+)
 
-sudo apt-get update
+if [ $(which apt-get) ]; then
+	sudo add-apt-repository ppa:neovim-ppa/unstable
+	sudo add-apt-repository ppa:git-core/ppa
+	sudo add-apt-repository ppa:webupd8team/java
 
-sudo apt-get install -y neovim \ # ppa:neovim-ppa
-						git \ # ppa:git-core
-						oracle-java8-installer \ # ppa:webupd8team
-						newsbeuter \ # random userland
-						postgresql libpq-dev
-						avrdude gcc-avr avr-libc emacs jq # for atreus firmware
+	sudo apt-get update
+
+	for target in ${TARGETS[@]}; do
+		sudo apt-get install -y $target
+	done
+
+	# sudo apt-get install -y neovim \ # ppa:neovim-ppa
+	# 						git \ # ppa:git-core
+	# 						oracle-java8-installer \ # ppa:webupd8team
+	# 						newsbeuter \ # random userland
+	# 						postgresql libpq-dev \
+	# 						avrdude gcc-avr avr-libc emacs jq # for atreus firmware
+fi
