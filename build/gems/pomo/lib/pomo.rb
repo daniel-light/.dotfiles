4c5cebf90690ad7590f39f087c1633e624da8c39
Daniel Light <daniel.sj.light@gmail.com>
2017-01-18 14:56:55 -0500

    Add tests to Pomo::Timer

--- build/gems/pomo/lib/pomo.rb
+++ build/gems/pomo/lib/pomo.rb
@@ -1,19 +1,28 @@
 require 'pomo/version'
 require 'pomo/timer'
 
-# This class, when run, tracks a pomodoro alternating between a 32 minute work
-# period and an 8 minute break period. It outputs to standard out and a file
 module Pomo
+  # This class, when run, tracks a pomodoro alternating between a 32 minute work
+  # period and an 8 minute break period. It outputs to standard out and a file
   class Runner
+    SIG_ROTATE = 'SIGUSR1'.freeze
+
     def initialize
       @timer = Timer.new
+      @pomo_file_name = '/tmp/pomo.timer'
+      @sounds_dir = ENV['HOME']
     end
 
     def run
-      trap_term
-      while sleep(3)
-        timer.switch_period if timer.elapsed_time > timer.period_length
-        timer.write_formatted_string
+      trap_signals
+
+      while sleep(1)
+        if timer.period_complete?
+          timer.switch_period
+          play_sound(timer.period_type)
+        end
+
+        write_formatted_string
       end
 
       nil
@@ -21,64 +30,31 @@ module Pomo
 
     private
 
-    attr_accessor :timer
+    attr_accessor :timer, :pomo_file_name, :sounds_dir
 
-    def trap_term
-      on_trap = proc do
+    def trap_signals
+      on_termination = proc do
         puts 'Removing pomo timer file'
-        timer.cleanup
+        cleanup
         exit
       end
 
-      Signal.trap('TERM', &on_trap)
-      Signal.trap('INT', &on_trap)
+      Signal.trap('TERM', &on_termination)
+      Signal.trap('INT', &on_termination)
 
-      nil
-    end
-  end
+      on_rotate = proc do
+        puts "Changing period in response to #{SIG_ROTATE}"
+        timer.switch_period
+      end
 
-  class Pomo
-    attr_accessor :period_type, :period_start
-    attr_reader :pomo_file_name
-
-    PERIOD_LENGTH = {
-      work: 60 * 32, # seconds
-      break: 60 * 8, # seconds
-    }.freeze
-
-    def initialize
-      self.period_type = :work
-      self.period_start = Time.now
-      @pomo_file_name = '/tmp/pomo.timer'
-    end
-
-    def period_length
-      PERIOD_LENGTH[period_type]
-    end
-
-    def elapsed_time
-      Time.now - period_start
-    end
-
-    def remaining_time
-      PERIOD_LENGTH[period_type] - elapsed_time
-    end
-
-    def formatted_clock
-      Time.at(remaining_time).strftime('%M:%S')
-    end
-
-    def switch_period
-      self.period_type = (period_type == :work ? :break : :work)
-      self.period_start = Time.now
-      play_sound("#{ENV['HOME']}/#{period_type}_start.mp3")
+      Signal.trap(SIG_ROTATE, &on_rotate)
 
       nil
     end
 
     def write_formatted_string
       File.open(pomo_file_name, 'w') do |file|
-        time_string = "#{formatted_clock} #{period_type} 🍅"
+        time_string = "#{timer.formatted_clock} #{timer.period_type} 🍅"
         puts time_string
         file.write(time_string)
       end
@@ -86,37 +62,15 @@ module Pomo
       nil
     end
 
-    def run
-      trap_term
-      while sleep(3)
-        switch_period if elapsed_time > period_length
-        write_formatted_string
-      end
+    def play_sound(period)
+      file = "#{sounds_dir}/pomo_#{period}_start.mp3"
+      Process.detach(fork { system("mpv #{file}") }) if File.exist?(file)
 
       nil
     end
 
     def cleanup
       File.delete(pomo_file_name) if File.exist?(pomo_file_name)
-
-      nil
-    end
-
-    def trap_term
-      on_trap = proc do
-        puts 'Removing pomo timer file'
-        cleanup
-        exit
-      end
-
-      Signal.trap('TERM', &on_trap)
-      Signal.trap('INT', &on_trap)
-
-      nil
-    end
-
-    def play_sound(filename)
-      Process.detach(fork { system("mpv #{filename}") }) if File.exist?(filename)
 
       nil
     end
--- build/gems/pomo/lib/pomo/timer.rb
+++ build/gems/pomo/lib/pomo/timer.rb
@@ -1,4 +1,5 @@
 module Pomo
+  # This class holds the logic for the actual timer.
   class Timer
     attr_accessor :period_type, :period_start
     attr_reader :pomo_file_name
@@ -11,7 +12,6 @@ module Pomo
     def initialize
       self.period_type = :work
       self.period_start = Time.now
-      @pomo_file_name = '/tmp/pomo.timer'
     end
 
     def period_length
@@ -30,32 +30,13 @@ module Pomo
       Time.at(remaining_time).strftime('%M:%S')
     end
 
+    def period_complete?
+      remaining_time <= 0
+    end
+
     def switch_period
       self.period_type = (period_type == :work ? :break : :work)
       self.period_start = Time.now
-      play_sound("#{ENV['HOME']}/#{period_type}_start.mp3")
-
-      nil
-    end
-
-    def write_formatted_string
-      File.open(pomo_file_name, 'w') do |file|
-        time_string = "#{formatted_clock} #{period_type} 🍅"
-        puts time_string
-        file.write(time_string)
-      end
-
-      nil
-    end
-
-    def cleanup
-      File.delete(pomo_file_name) if File.exist?(pomo_file_name)
-
-      nil
-    end
-
-    def play_sound(filename)
-      Process.detach(fork { system("mpv #{filename}") }) if File.exist?(filename)
 
       nil
     end
